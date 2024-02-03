3876c6c58243de3de7aa282869b2d98038a14fa4
Chris <c17122947@gmail.com>
2024-02-03 16:38:27 -0600

    Finished first RPS

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function getComputerChoice(){
     return choice;
 }
 
+
 function checkWinner(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         return "Tie";
@@ -36,7 +37,48 @@ function playRound(playerselection, computerselection){
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
-console.log(`Player=${playerSelection}, Computer=${computerSelection}`)
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+    const choice = prompt ("Rock Paper Scissors");
+        if(choice == null){
+                continue; 
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;   
+        }
+    }
+}
+ 
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log('-------')
+        if(checkWinner(playerSelection, computerSelection) == "Player"){
+        scorePlayer++;
+        }   
+        else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+        scoreComputer++;   
+        }
+    }
+    console.log("Game Over")
+    if (scorePlayer > scoreComputer){
+    console.log("Player is the winner!")
+    }
+    else if (scoreComputer > scorePlayer){
+    console.log("Computer is the winner!")
+    }
+    else {
+    console.log("Tie!!!")
+    }
+}
+
+
+game()
